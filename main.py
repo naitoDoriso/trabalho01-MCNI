b6bfc3b0f53c915684e1cf3a9c50e2c068e39cc1
naitoDoriso <89417602+naitoDoriso@users.noreply.github.com>
2025-04-26 22:51:57 -0300

    +Poucos pontos fixos, Slides refeitos em Latex

--- main.py
+++ main.py
@@ -3,140 +3,9 @@ import numpy as np
 import scipy.sparse as sp
 import scipy.linalg as la
 import time
+import os
 
-## Coordenadas
-P = []
-
-f = open("manh.xy", 'r')
-for line in f:
-	if line.strip():
-		P.append(list(map(float, line.strip().split())))
-P = np.array(P)
-f.close()
-
-## Matriz de adjacencia
-n = P.shape[0]
-
-A = np.zeros((n,n))
-E = []
-
-f = open("manh.el", 'r')
-for line in f:
-	if line.strip():
-		i, j = map(int, line.strip().split())
-		E.append([i,j])
-		A[i,j] = 1
-		A[j,i] = 1
-E = np.array(E)
-f.close()
-
-## Plotando o grafo desconexo
-f = plt.figure(1)
-
-for e in E:
-    plt.plot(P[e,0], P[e,1], color="lightsteelblue", linewidth=0.5) # Plotando arestas
-
-plt.scatter(P[:,0], P[:,1], c="darkgrey", s=1) # Plotando os vertices
-
-plt.title("Grafo desconexo")
-plt.axis("equal")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
-
-f.savefig("fig1.pdf")
-
-## Plotando todas as componentes conexas
-nc, idx = sp.csgraph.connected_components(sp.csr_matrix(A), directed=False)
-
-f = plt.figure(2,figsize=(15,12))
-
-for i in range(0,nc):
-	plt.subplot(2, 3, i+1)
-
-	c = np.where(idx == i)[0]
-
-	Pc = P[c,:]
-	Ac = A[np.ix_(c,c)]
-	Ec = np.array(np.where(np.triu(Ac)==1)).T
-
-	for e in Ec:
-		plt.plot(Pc[e,0], Pc[e,1], color="lightsteelblue", linewidth=0.5) # Plotando arestas
-
-	plt.scatter(Pc[:,0], Pc[:,1], c="darkgrey", s=1) # Plotando os vertices
-
-	plt.title(f"Componente conexa {i+1}")
-	plt.axis("equal")
-	plt.xlabel('x')
-	plt.ylabel('y')
-	plt.grid(True)
- 
-f.savefig("fig2.pdf")
-
-## Maior componente conexa
-bc = np.where(idx == np.argmax(np.bincount(idx)))[0] # maior componente conexa
-
-P = P[bc,:]
-A = A[np.ix_(bc,bc)]
-E = np.array(np.where(np.triu(A)==1)).T
-
-# Plotando maior componente conexa
-f = plt.figure(3)
-
-for e in E:
-    plt.plot(P[e,0], P[e,1], color="lightsteelblue", linewidth=0.5) # Plotando arestas
-
-plt.scatter(P[:,0], P[:,1], c="darkgrey", s=1) # Plotando os vertices
-
-plt.title("Maior componente conexa")
-plt.axis("equal")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
-
-f.savefig("fig3.pdf")
-
-## Condicao de contorno
-n = A.shape[0]
-k = int(5/100 * n) # 5% dos pontos
-
-np.random.seed(42)
-idx = np.random.choice(np.arange(0, n), size=k, replace=False) # Escolhendo 5% de pontos para condicao de contorno
-b = np.zeros((n,1))
-b[idx,0] = np.random.uniform(20, 30, size=k) # Escolhendo temperaturas T, com 20°C <= T < 30°C para condicao de contorno
-
-## Plotando a maior componente conexa com os pontos fixos
-f = plt.figure(4)
-
-for e in E:
-    plt.plot(P[e,0], P[e,1], color="gainsboro", linewidth=0.5) # Plotando arestas
-
-plt.scatter(P[:,0], P[:,1], c="darkgrey", s=1) # Plotando os vertices
-plt.scatter(P[idx,0], P[idx,1], c=b[idx], cmap="jet", s=2, zorder=2) # Plotando os vertices fixos
-plt.colorbar()
-
-plt.title(r"Temperatura fixa em 5% dos pontos")
-plt.axis("equal")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
-
-f.savefig("fig4.pdf")
-
-## Montando o sistema M*x = c
-G = np.diag(A.sum(axis=1)) # Matriz de grau
-L = G-A # Matriz Laplaciana
-
-alpha = 1.0e7
-MP = np.zeros((n,n)) # Matriz de penalidade
-MP[idx,idx] = alpha
-
-M = L+MP
-c = MP@b
-
-## Resolvendo o sistema por metodos diretos
-T = np.linalg.solve(M,c)
-
+# Funcoes dos metodos diretos
 def Cholesky(M, c):
     """
     Resolve o sistema M*x = c usando a decomposição de Cholesky.
@@ -183,49 +52,7 @@ def QR(M, c):
 	x = la.solve_triangular(R, Qtc, lower=False)
 	return x
 
-tempos = []
-metods = ["np.linalg.solve", "Cholesky", "LU", "QR"]
-
-#Tempo Metodo Direto
-beg= time.time()
-aux = np.linalg.solve(M,c)
-end = time.time()
-tempos.append(end-beg)
-
-#Tempo Metodo Cholesky
-beg1= time.time()
-T = Cholesky(M,c)
-end1 = time.time()
-tempos.append(end1-beg1)
-
-#Tempo Metodo LU
-beg2 = time.time()
-LU(M,c)
-end2 = time.time()
-tempos.append(end2-beg2)
-
-#Tempo Metodo QR
-beg3 = time.time()
-QR(M, c)
-end3 = time.time()
-tempos.append(end3 - beg3)
-
-## Plotando o grafico de barras comparando os tempos
-f = plt.figure(5, figsize=(10, 6))
-
-plt.bar(metods, tempos, color='skyblue')
-plt.ylabel('Tempo de execução (segundos)')
-plt.title('Comparação de tempo entre diferentes métodos de solução')
-plt.grid(True, axis='y', linestyle='--', alpha=0.7)
-
-# Adicionando o tempo em cima das barras
-for i, tempo in enumerate(tempos):
-    plt.text(i, tempo + max(tempos)*0.01, f'{tempo:.6f}',
-             ha='center', va='bottom', fontsize=9)
-
-f.savefig("fig5.pdf")
-
-## Resolvendo o sistema por metodos iterativos
+# Funcoes dos metodos iterativos
 def GaussJacobi(M,c):
 	n = M.shape[0]
 	D = np.diag(np.diag(M))
@@ -234,7 +61,7 @@ def GaussJacobi(M,c):
 
 	erro = 0
 	k = 0
-	kmax = 1e4
+	kmax = 1e5
 	x0 = np.zeros((n,1))
 	while (np.linalg.norm(c-M@x0) > tol and k < kmax):
 		k = k+1
@@ -252,7 +79,7 @@ def GaussSeidel(M,c):
 
 	erro = 0
 	k = 0
-	kmax = 1e4
+	kmax = 1e5
 	x0 = np.zeros((n,1))
 	while (np.linalg.norm(c-M@x0) > tol and k < kmax):
 		k = k+1
@@ -267,14 +94,14 @@ def GradConj(M,c):
 	r0 = c-(M @ x0)
 	p0 = r0
 
-	a = (r0.T @ r0)/(r0.T @ M @ r0)
+	a = (r0.T @ r0)/(p0.T @ M @ p0)
 	x = x0 + a*p0
 	r = r0 - a*(M @ p0)
 	p = p0
 
 	erro = 0
 	k = 1
-	kmax = 1e4
+	kmax = 1e5
 	while (np.linalg.norm(r) > tol and k < kmax):
 		x0 = x
 		p0 = p
@@ -292,53 +119,283 @@ def GradConj(M,c):
 		erro = 1
 	return [x,k,erro]
 
-## Calcundo o numero de iteracoes e os tempos
-tol = 1.0e-3
+# Coordenadas
+P = []
 
-st = time.time()
-GaussJacobi(M,c)
-end = time.time()
-TJ = end-st
+f = open("manh.xy", 'r')
+for line in f:
+	if line.strip():
+		P.append(list(map(float, line.strip().split())))
+P = np.array(P)
+f.close()
 
-st = time.time()
-GaussSeidel(M,c)
-end = time.time()
-TS = end-st
+# Matriz de adjacencia
+n = P.shape[0]
 
-st = time.time()
-GradConj(M,c)
-end = time.time()
-TG = end-st
+A = np.zeros((n,n))
 
-f = plt.figure(6)
+f = open("manh.el", 'r')
+for line in f:
+	if line.strip():
+		i, j = map(int, line.strip().split())
+		A[i,j] = 1
+		A[j,i] = 1
+f.close()
 
+# Plotando o grafo desconexo
+f = plt.figure(1)
+
+E = np.array(np.where(np.triu(A) == 1)).T
 for e in E:
-    plt.plot(P[e,0], P[e,1], color="gainsboro", linewidth=0.5) # Plotando arestas
+    plt.plot(P[e,0], P[e,1], color="lightsteelblue", linewidth=0.5) # Arestas
 
-plt.scatter(P[:,0], P[:,1], c=T, cmap="jet", s=1, zorder=2) # Plotando os vertices
-plt.colorbar()
+plt.scatter(P[:,0], P[:,1], c="darkgrey", s=1) # Vertices
 
-plt.title(r"Temperatura interpolada")
+plt.title("Grafo desconexo")
 plt.axis("equal")
 plt.xlabel('x')
 plt.ylabel('y')
 plt.grid(True)
 
-f.savefig("fig6.pdf")
+if not os.path.exists("figs"):
+    os.makedirs("figs")
 
-f = plt.figure(7, figsize=(10, 6))
+f.savefig("figs/fig1.pdf")
 
-tempos = [TJ,TS,TG]
-plt.bar(["Gauss Jacobi", "Gauss Seidel", "Gradientes Conjugados"], tempos, color='skyblue')
-plt.ylabel('Tempo de execução (segundos)')
-plt.title('Comparação de tempo entre diferentes métodos de solução')
-plt.grid(True, axis='y', linestyle='--', alpha=0.7)
+# Plotando todas as componentes conexas
+nc, idx = sp.csgraph.connected_components(sp.csr_matrix(A), directed=False)
 
-# Adicionando o tempo em cima das barras
-for i, tempo in enumerate(tempos):
-    plt.text(i, tempo + max(tempos)*0.01, f'{tempo:.6f}',
-             ha='center', va='bottom', fontsize=9)
+f = plt.figure(2,figsize=(23,14))
 
-f.savefig("fig7.pdf")
+for i in range(0,nc):
+	plt.subplot(2, 3, i+1)
 
-plt.show()
+	c = np.where(idx == i)[0]
+
+	Pc = P[c,:]
+	Ac = A[np.ix_(c,c)]
+	Ec = np.array(np.where(np.triu(Ac)==1)).T
+
+	for e in Ec:
+		plt.plot(Pc[e,0], Pc[e,1], color="lightsteelblue", linewidth=0.5) # Arestas
+
+	plt.scatter(Pc[:,0], Pc[:,1], c="darkgrey", s=1) # Vertices
+
+	plt.title(f"Componente conexa {i+1}")
+	plt.axis("equal")
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.grid(True)
+ 
+f.savefig("figs/fig2.pdf")
+
+bc = np.where(idx == np.argmax(np.bincount(idx)))[0] # Maior componente conexa
+
+P = P[bc,:]
+A = A[np.ix_(bc,bc)]
+E = np.array(np.where(np.triu(A)==1)).T
+
+# Plotando maior componente conexa
+f = plt.figure(3)
+
+for e in E:
+    plt.plot(P[e,0], P[e,1], color="lightsteelblue", linewidth=0.5) # Arestas
+
+plt.scatter(P[:,0], P[:,1], c="darkgrey", s=1) # Vertices
+
+plt.title("Maior componente conexa")
+plt.axis("equal")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+
+f.savefig("figs/fig3.pdf")
+
+def solve(i0, b, ix):
+	# Condicao de contorno
+	n = A.shape[0]
+	k = ix.shape[0]
+
+	# Plotando a maior componente conexa com os pontos fixos
+	f = plt.figure(i0,figsize=(10, 6))
+
+	for e in E:
+		plt.plot(P[e,0], P[e,1], color="gainsboro", linewidth=0.5) # Arestas
+
+	plt.scatter(P[:,0], P[:,1], c="darkgrey", s=1) # Vertices
+	plt.scatter(P[ix,0], P[ix,1], c=b[ix], cmap="jet", s=2, zorder=2) # Vertices fixos
+	plt.colorbar()
+
+	plt.title(f"Temperatura fixa em {k} pontos de um total de {n}")
+	plt.axis("equal")
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.grid(True)
+
+	f.savefig(f"figs/fig{i0}.pdf")
+
+	# Montando o sistema
+	G = np.diag(A.sum(axis=1)) # Matriz de grau
+	L = G-A # Matriz Laplaciana
+
+	alpha = 1.0e7
+	MP = np.zeros((n,n)) # Matriz de penalidade
+	MP[ix,ix] = alpha
+
+	M = L+MP # M = (L+P)
+	c = MP@b # c = Pb
+	# (L+P)T = Pb <=> M * T = c
+
+	# Resolvendo o sistema por metodos diretos
+	tempos = []
+	metods = ["np.linalg.solve", "Cholesky", "LU", "QR"]
+
+	beg= time.time()
+	Td = np.linalg.solve(M,c)
+	end = time.time()
+	tempos.append(end-beg)
+
+	beg1= time.time()
+	Tc = Cholesky(M,c)
+	end1 = time.time()
+	tempos.append(end1-beg1)
+
+	beg2 = time.time()
+	Tl = LU(M,c)
+	end2 = time.time()
+	tempos.append(end2-beg2)
+
+	beg3 = time.time()
+	Tq = QR(M, c)
+	end3 = time.time()
+	tempos.append(end3 - beg3)
+
+	# Plotando o grafico de barras, comparando os tempos dos metodos diretos
+	f = plt.figure(i0+1, figsize=(10, 6))
+
+	plt.bar(metods, tempos, color='skyblue')
+	plt.ylabel('Tempo de execução (segundos)')
+	plt.title('Comparação de tempo entre métodos diretos')
+	plt.grid(True, axis='y', linestyle='--', alpha=0.7)
+
+	# Adicionando o tempo em cima das barras
+	for i, tempo in enumerate(tempos):
+		plt.text(i, tempo + max(tempos)*0.01, f'{tempo:.6f}',
+				ha='center', va='bottom', fontsize=9)
+
+	f.savefig(f"figs/fig{i0+1}.pdf")
+
+	# Resolvendo o sistema por metodos iterativos
+	passos = []
+	tempos = []
+	metods = ["Gauss Jacobi", "Gauss Seidel", "Gradientes Conjugados"]
+
+	st = time.time()
+	[Tj,k,_] = GaussJacobi(M,c)
+	end = time.time()
+	passos.append(k)
+	tempos.append(end-st)
+
+	st = time.time()
+	[Ts,k,_] = GaussSeidel(M,c)
+	end = time.time()
+	passos.append(k)
+	tempos.append(end-st)
+
+	st = time.time()
+	[Tg,k,_] = GradConj(M,c)
+	end = time.time()
+	passos.append(k)
+	tempos.append(end-st)
+
+	# Plotando o grafico de barras, comparando os tempos e passos dos metodos iterativos
+	f = plt.figure(i0+2, figsize=(10, 6))
+
+	plt.bar(metods, tempos, color='skyblue')
+	plt.ylabel('Tempo de execução (segundos)')
+	plt.title('Comparação de tempo entre métodos iterativos')
+	plt.grid(True, axis='y', linestyle='--', alpha=0.7)
+
+	# Adicionando o tempo em cima das barras
+	for i, tempo in enumerate(tempos):
+		plt.text(i, tempo + max(tempos)*0.01, f'{tempo:.6f}',
+				ha='center', va='bottom', fontsize=9)
+	
+	print(passos) # Mostra a quantidade de passos necessarios para convergencia dos 3 metodos
+
+	f.savefig(f"figs/fig{i0+2}.pdf")
+
+	# Plotando a interpolacao no grafico
+	T = Tc # Cholesky
+	f = plt.figure(i0+3)
+
+	for e in E:
+		plt.plot(P[e,0], P[e,1], color="gainsboro", linewidth=0.5) # Arestas
+
+	plt.scatter(P[:,0], P[:,1], c=T, cmap="jet", s=1, zorder=2) # Vertices
+	plt.colorbar()
+
+	plt.title("Temperatura interpolada")
+	plt.axis("equal")
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.grid(True)
+
+	f.savefig(f"figs/fig{i0+3}.pdf")
+
+	# Analisando o erro dos metodos diretos
+	f = plt.figure(i0+4, figsize=(10, 6))
+
+	r = np.array([np.linalg.norm(c-M@Td), np.linalg.norm(c-M@Tc), np.linalg.norm(c-M@Tl), np.linalg.norm(c-M@Tq)])
+	plt.bar(["np.linalg.solve", "Cholesky", "LU", "QR"], r, color='skyblue')
+	plt.ylabel('Erro')
+	plt.yscale('log', base=10)
+	plt.title('Comparação do resíduo entre métodos diretos')
+	plt.grid(True, axis='y', linestyle='--', alpha=0.7)
+
+	# Adicionando o erro em cima das barras
+	for i, x in enumerate(r):
+		plt.text(i, x + max(r)*0.01, f'{x:.7f}',
+				ha='center', va='bottom', fontsize=9)
+
+	f.savefig(f"figs/fig{i0+4}.pdf")
+
+	# Analisando o erro dos metodos iterativos
+	f = plt.figure(i0+5, figsize=(10, 6))
+
+	r = np.array([np.linalg.norm(c-M@Tj), np.linalg.norm(c-M@Ts), np.linalg.norm(c-M@Tg)])
+	plt.bar(["Gauss Jacobi", "Gauss Seidel", "Gradientes Conjugados"], r, color='skyblue')
+	plt.ylabel('Erro')
+	plt.yscale('log', base=10)
+	plt.title('Comparação do resíduo entre métodos iterativos')
+	plt.grid(True, axis='y', linestyle='--', alpha=0.7)
+
+	# Adicionando o erro em cima das barras
+	for i, x in enumerate(r):
+		plt.text(i, x + max(r)*0.01, f'{x:.6f}',
+				ha='center', va='bottom', fontsize=9)
+
+	f.savefig(f"figs/fig{i0+5}.pdf")
+
+# Escolhendo 9 pontos fixos manualmente
+n = A.shape[0]
+P_ord = np.argsort(P[:,1])[::-1] # Ordena em ordem decrescente de acordo com y
+
+aux = np.linspace(0,n-4,9,dtype=int)
+ix = P_ord[aux] # Pega 9 pontos, em ordem decrescente de acordo com o y
+b = np.zeros((n,1))
+b[ix,0] = np.linspace(20,24,9) # Atribui as menores temperaturas para os mais altos e, as maiores para os mais baixos
+
+tol = 1.0e-1
+solve(4, b, ix) # Figura 4 ate Figura 9
+
+# Escolhendo 5% de pontos fixos aleatoriamente
+np.random.seed(42)
+k = int(5/100 * n)
+
+ix = np.random.choice(np.arange(0, n), size=k, replace=False) # Escolhendo 5% de pontos para condicao de contorno
+b = np.zeros((n,1))
+b[ix,0] = np.random.uniform(20, 24, size=k) # Escolhendo temperaturas T, com 20°C <= T < 24°C para condicao de contorno
+
+tol = 1.0e-3
+solve(10, b, ix) # Figura 10 ate Figura 15
